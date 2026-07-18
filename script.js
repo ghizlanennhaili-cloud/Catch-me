240005ac0008c22a4958f6820ce97a8a17fd1ee2
ghizlanennhaili-cloud <ghizlanennhaili@gmail.com>
2026-07-18 13:01:23 +0100

    Mise à jour du design et texte

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const phrases = [
     "Catch me? 😜", "Faster! 🏃‍♀️", "Too slow! 😏", "Try again! 😮", 
-    "You can't catch me! 💪", "I'm here! 😊", "Catch me if you can!😜",
+    "You can't catch me! 💪", "I'm here! 😊", "Catch me if you can! 😜",
     "So close... 🤏", "Nice try! 😉", "Keep running! 🏃‍♂️", 
-    "Almost got me! 😝", "Still here! 🍭",, "Where are you? 🧐", "Getting closer! 🥵", "Too fast for you! 😎", 
-    "Catch me! 😆", "Can't touch this! 💅", "Try harder! 🔥", 
-    "Oops! Missed me! 😅", "Still chasing? 😆", "Look at me! 😏", "Give up! 🏳️",
-    "Never catch me! 😈"
+    "Almost got me! 😝", "Still here! 🍭", "Where are you? 🧐", 
+    "Getting closer! 🥵", "Too fast for you! 😎", "Catch me! 😆", 
+    "Can't touch this! 💅", "Try harder! 🔥", "Oops! Missed me! 😅", 
+    "Still chasing? 😆", "Look at me! 😏", "Give up! 🏳️", "Never catch me! 😈"
 ];
 
 const textElement = document.getElementById('catch-text');
@@ -30,13 +30,26 @@ function createEmoji() {
 }
 setInterval(createEmoji, 2000);
 
-// Interaction : le texte s'enfuit quand on l'approche
-textElement.addEventListener('mouseover', () => {
-    const randomX = Math.random() * (window.innerWidth - 150);
-    const randomY = Math.random() * (window.innerHeight - 50);
-    textElement.style.left = randomX + 'px';
-    textElement.style.top = randomY + 'px';
-    
-    // Change la phrase
-    textElement.innerText = phrases[Math.floor(Math.random() * phrases.length)];
+// Interaction : Rétrécissement à l'approche et fuite au contact
+document.addEventListener('mousemove', (e) => {
+    const rect = textElement.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const distance = Math.sqrt(Math.pow(e.clientX - centerX, 2) + Math.pow(e.clientY - centerY, 2));
+
+    // Si la souris est proche (moins de 200px), on réduit la taille
+    if (distance < 200) {
+        textElement.classList.add('text-small');
+    } else {
+        textElement.classList.remove('text-small');
+    }
+
+    // Si on est vraiment très près (moins de 50px), le texte "s'enfuit"
+    if (distance < 50) {
+        const randomX = Math.random() * (window.innerWidth - 150);
+        const randomY = Math.random() * (window.innerHeight - 50);
+        textElement.style.left = randomX + 'px';
+        textElement.style.top = randomY + 'px';
+        textElement.innerText = phrases[Math.floor(Math.random() * phrases.length)];
+    }
 });
